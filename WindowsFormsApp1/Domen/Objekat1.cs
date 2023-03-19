13bf3d29566411014ff87754f77013558b15bf34
resenja <121758711+resenja@users.noreply.github.com>
2023-03-19 11:27:51 +0100

    Add files via upload

---
+++ WindowsFormsApp1/Domen/Objekat1.cs
@@ -0,0 +1,12 @@
+﻿namespace Domen
+{
+    public class Objekat1
+    {
+        public int id { get; set; }
+        public string atribut1 { get; set; }
+        public override string ToString()
+        {
+            return string.Format("{0} {1}", id, atribut1);
+        }
+    }
+}
